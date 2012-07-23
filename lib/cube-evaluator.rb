ced11a320622e509a5d68b9ae76a7e3c6eaa5271
Matteo Depalo <matteodepalo@gmail.com>
2012-07-23 10:26:08 +0200

    initial commit

---
+++ lib/cube-evaluator.rb
@@ -0,0 +1,7 @@
+require "cube-evaluator/version"
+
+module Cube
+  module Evaluator
+    # Your code goes here...
+  end
+end
---
+++ lib/cube-evaluator/version.rb
@@ -0,0 +1,5 @@
+module Cube
+  module Evaluator
+    VERSION = "0.0.1"
+  end
+end
